7b2867e6d7f416514b85ada766efeea0bbe1f2b4
Leon <programmerleon@gmail.com>
2026-07-08 11:30:00 -0400

    feat(core): log unhandled UI and AppDomain exceptions
    Unhandled exceptions previously crashed silently with no trace in
    the log file, making post-mortem debugging of user-reported crashes
    difficult.

--- src/DrawingTree/App.xaml.cs
+++ src/DrawingTree/App.xaml.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Configuration;
 using System.Data;
+using System.Windows;
+using System.Windows.Threading;
+using DrawingTree.Logging;
 
 namespace DrawingTree;
 
@@ -8,4 +12,21 @@ namespace DrawingTree;
 /// &lt;/summary&gt;
 public partial class App : System.Windows.Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += OnAppDomainUnhandledException;
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        Logger.Instance.Error($"Unhandled UI exception: {e.Exception}");
+    }
+
+    private void OnAppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Logger.Instance.Error($"Unhandled exception (terminating={e.IsTerminating}): {e.ExceptionObject}");
+    }
 }
